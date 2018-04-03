867d5cdbfec63bc0135169909453ef68028d0320
Leonard Bos <leonard442@hotmail.com>
2018-04-03 10:59:45 +0200

    First commit (Test)

---
+++ app/src/main/java/com/example/leonard/imagecheck/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.leonard.imagecheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
